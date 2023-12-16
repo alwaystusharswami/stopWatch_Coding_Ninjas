ecf040e71d5ec7c66298f8871c4aa2e680fef2c6
tushar <swamitusharjee2018@gmail.com>
2023-12-16 19:30:54 +0530

    all functionality done on 3 button now we add style in our website

--- script.js
+++ script.js
@@ -5,56 +5,71 @@ const reset = document.querySelector("#reset");
 let hours = document.querySelector("#hour");
 let minute = document.querySelector("#minute");
 let seconds = document.querySelector("#sec");
+let isPaused=false;
 
 let min = 0;
 let sec = 0;
 let hour = 0;
 
-let interval;
-
-start.addEventListener("click", function () {
-  // ! using set interval function
-  interval = setInterval(function () {
-    // * increase second if sec greater than 59s
+let  interval = window.setInterval(function () {
+    if(isPaused){
+        // * increase second if sec greater than 59s
     if (sec >= 59) {
-      // * increase minute if minute greater than 59s
-
-      if (min >= 59) {
-        // * increase hour  and set minute to 0
-
-        hour++;
-        min = 0;
-        if (hour < 10) {
-          hours.innerHTML = "0" + hour;
-          minute.innerHTML = "0" + min;
+        // * increase minute if minute greater than 59s
+  
+        if (min >= 59) {
+          // * increase hour  and set minute to 0
+  
+          hour++;
+          min = 0;
+          if (hour < 10) {
+            hours.innerHTML = "0" + hour;
+            minute.innerHTML = "0" + min;
+          } else {
+            hours.innerHTML = hour;
+            minute.innerHTML = "0" + min;
+          }
         } else {
-          hours.innerHTML = hour;
-          minute.innerHTML = "0" + min;
+          // * increase min if min less than 59min
+          min++;
+          if (min < 10) {
+            minute.innerHTML = "0" + min;
+          } else {
+            minute.innerHTML = min;
+          }
+          sec = 0;
+          seconds.innerHTML = "0" + sec;
         }
       } else {
-        // * increase min if min less than 59min
-        min++;
-        if (min < 10) {
-          minute.innerHTML = "0" + min;
+        // * increase second if sec less than 59s
+  
+        sec++;
+        if (sec < 10) {
+          seconds.innerHTML = "0" + sec;
         } else {
-          minute.innerHTML = min;
+          seconds.innerHTML = sec;
         }
-        sec = 0;
-        seconds.innerHTML = "0" + sec;
-      }
-    } else {
-      // * increase second if sec less than 59s
-
-      sec++;
-      if (sec < 10) {
-        seconds.innerHTML = "0" + sec;
-      } else {
-        seconds.innerHTML = sec;
       }
     }
   }, 1000);
+
+start.addEventListener("click", function (e) {
+  // ! using set interval function
+  e.preventDefault();
+  isPaused=true;
+
+ 
 });
 
+pause.addEventListener('click',function(){
+    isPaused=!isPaused;
+    if(isPaused){
+        pause.innerHTML='pause';
+    }else{
+        pause.innerHTML='continue';
+
+    }
+})
 
 reset.addEventListener("click", function () {
     clearInterval(interval);
